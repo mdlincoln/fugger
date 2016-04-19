8cff10a0e16512e4d7cdccd894d6908240902218
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-04-19 14:25:41 -0400

    b&w filter

--- R/filters.R
+++ R/filters.R
@@ -10,3 +10,17 @@ fug_gs <- function(p) {
   plot(gs_raster)
   invisible(gs_raster)
 }
+
+#' Black and white filter
+#'
+#' Run a grayscale filter on a plot
+#'
+#' @param plot An object or expression that evaluates as a plot.
+#' @param threshold Set a threshold for black vs. white. Defaults to "auto". See \link{threshold}.
+#' @export
+fug_bw <- function(p, threshold = "auto") {
+  raster_plot <- rasterize(p)
+  bw_raster <- imager::threshold(imager::grayscale(raster_plot), thr = threshold)
+  plot(bw_raster)
+  invisible(bw_raster)
+}
